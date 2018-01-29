f6bd24e2cf9afb9f3ecf984ae2bfe0b14444c230
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-29 17:37:19 +0200

    fix auth flow

--- server/config/keys.js
+++ server/config/keys.js
@@ -5,6 +5,3 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./dev');
 }
 
-1074114855134-abs8qf02nuje69cmbn65ts9re1fsec8i.apps.googleusercontent.com
-
-37AVxo8JCU5OGZI8-c4IBa51
